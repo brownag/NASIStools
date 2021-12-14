766920709296452ef44fdc89395e6cbf3329d455
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-12-14 10:04:08 -0800

    Skip tests that require NASIS (for now)

--- tests/testthat/test-code_converters.R
+++ tests/testthat/test-code_converters.R
@@ -1,4 +1,7 @@
 test_that("musym_to_all works", {
+
+  skip_if_not(soilDB::local_NASIS_defined())
+
   expect_true(is.vector(musym_to_nmusym("asdf")))
   expect_true(is.vector(musym_to_muiid("asdf")))
   expect_true(is.vector(musym_to_dmuiid("asdf")))
