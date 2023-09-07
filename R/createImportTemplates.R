bbfc85007584b7b2ca0bd762a45bb68dd3b5958c
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-09-06 19:56:06 -0700

    create_import_template: fix for multiple instances of the same column name

--- R/createImportTemplates.R
+++ R/createImportTemplates.R
@@ -27,11 +27,13 @@ create_import_template <- function(.data,
   stopifnot(endsWith(file, ".csv") || endsWith(file, ".xlsx"))
 
   as_xlsx <- endsWith(file, ".xlsx")
-
-  x <- c(paste0(c(template_name, template_version, rep("", length(columns) - 1)), collapse = delimeter),
+  
+  x <- c(paste0(c(template_name, template_version, rep("", length(columns) - 1)),
+                collapse = delimeter),
          paste0(rep(delimeter, length(columns)), collapse = ""),
-         paste0(paste0(gsub("_", " ", columns), collapse = delimeter)),
-         paste0(apply(.data[, columns, drop = FALSE], 1, paste0, collapse = delimeter), delimeter))
+         paste0(paste0(columns, collapse = delimeter)),
+         paste0(apply(as.matrix(.data)[, colnames(.data) %in% columns, drop = FALSE], 
+                      MARGIN = 1, FUN = paste0, collapse = delimeter), delimeter))
   m <- lapply(x, function(x) strsplit(x, split = delimeter, fixed = TRUE)[[1]])
   mat <- do.call('rbind', m)
   mat[is.na(mat)] <- ""
